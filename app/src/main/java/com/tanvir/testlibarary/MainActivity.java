cd89a55ebd4d6f406643f4ddafa24ee9e9ff92b9
Tanvir Ahmed <tanvir3488@gmail.com>
2019-09-16 17:50:32 +0600

    t

--- app/src/main/java/com/tanvir/testlibarary/MainActivity.java
+++ app/src/main/java/com/tanvir/testlibarary/MainActivity.java
@@ -3,8 +3,6 @@ package com.tanvir.testlibarary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.tanvir.test_library.Math;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
