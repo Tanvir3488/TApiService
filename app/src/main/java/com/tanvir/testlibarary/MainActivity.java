9b7463f9c8319c59dd067d38c8e9a0aafc3bd20f
Tanvir Ahmed <tanvir3488@gmail.com>
2019-12-24 17:13:51 +0600

    new request method

---
+++ app/src/main/java/com/tanvir/testlibarary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tanvir.testlibarary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Math.Minuse(1,2);
+        Math.Minuse(1,2);
+        Math.Minuse(1,2);
+        Math.Minuse(1,2);
+    }
+}
